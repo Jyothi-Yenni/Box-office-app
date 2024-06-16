f1651a733f582802f26a5dd23e0a991cb8b717d5
Jyothi <jyenni665@gmail.com>
2024-06-16 12:39:34 +0530

    used useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,45 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
 
-  // console.log(searchOption);
-
-  //console.log(apiDataError);
+  const { data: apiData, error: apiDataError} = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () => 
+      filter.searchOption === 'shows' 
+      ? searchForShows(filter.q) 
+      : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
 
   const onSearch = async ({q, searchOption}) => {
-
-    try {
-      setApiDataError(null);
-
-     let result;
-      if(searchOption === 'shows'){
-         result = await searchForShows(q);
-      setApiData(result);
-      
-      } else{
-         result = await searchForPeople(q);
-        setApiData(result);
-      }
-
-
-    } catch (error) {
-      setApiDataError(error);
-    }
-    //console.log(apiData);
+    setFilter({ q, searchOption })
   };
+// const [apiData, setApiData] = useState(null);
+// const [apiDataError, setApiDataError] = useState(null);
+// console.log(searchOption);
+//console.log(apiDataError);
 
+  //   try {
+  //     setApiDataError(null);
+  //    let result;
+  //     if(searchOption === 'shows'){
+  //        result = await searchForShows(q);
+  //     setApiData(result); 
+  //     } else{
+  //        result = await searchForPeople(q);
+  //       setApiData(result);
+  //     }
+  //   } catch (error) {
+  //     setApiDataError(error);
+  //   }
+  //   //console.log(apiData);
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Error occured: {apiDataError.message}</div>;
